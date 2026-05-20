35a14391be3c38f459003ecb5a127480e8efa891
jordan-lenchitz <jordan.lenchitz@gmail.com>
2026-05-20 06:05:16 +0000

    fix: force cache refresh with v2 identifiers and make stupid hub draggable

--- src/components/StupidGrafanaPanel.tsx
+++ src/components/StupidGrafanaPanel.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { DraggablePanel } from './DraggablePanel';
 
 export const StupidGrafanaPanel: React.FC = () => {
   return (
-    <div className="stupid-grafana-panel panel" style={{ width: '95vw', maxWidth: '1600px', margin: '0 auto' }}>
-      <div className="panel-header bg-darker">
+    <DraggablePanel className="stupid-grafana-panel">
+      <div className="panel-header bg-darker" style={{ borderBottom: '2px solid #8a2be2', cursor: 'move' }}>
         <span className="text-purple">🌀</span>
-        <span>absurdist stupid hub (maximum slop telemetry)</span>
-        <span className="header-right text-purple blink">● unstable</span>
+        <span className="text-purple" style={{ fontWeight: 'bold' }}>ABSURDIST STUPID HUB V2 (MAXIMUM SLOP)</span>
+        <span className="header-right text-purple blink">● unstable_v2</span>
       </div>
       <div 
         className="panel-content" 
-        style={{ padding: 0, height: '1200px', backgroundColor: '#111217' }} 
+        style={{ padding: 0, height: '600px', width: '900px', backgroundColor: '#111217' }} 
       >
         <iframe 
-          src="https://grafana-absurdist-336490534272.us-central1.run.app/d/absurdist-stupid?orgId=1&refresh=5s" 
+          src="https://grafana-absurdist-336490534272.us-central1.run.app/d/absurdist-stupid?orgId=1&refresh=5s&v=2" 
           width="100%" 
           height="100%" 
           frameBorder="0"
           title="stupid grafana"
         ></iframe>
       </div>
-    </div>
+    </DraggablePanel>
   );
 };
